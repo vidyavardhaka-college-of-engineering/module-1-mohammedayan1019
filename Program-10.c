4d546a4c5afd75b491afbb170d74b2c79ad1d1f8
mohammedayan101 <ayanmohammed232@gmail.com>
2021-03-24 11:37:49 +0000

    10

--- Program-10.c
+++ Program-10.c
@@ -6,9 +6,9 @@ int main()
 {
    float num, root;
    printf("enter the number\n");   // Correct the code
-   scanf("%f", &num);
+   scanf("%f",&num);
    // Computes the square root of num and stores in root.
    root = sqrt(num);
-   printf("Square root of %.2f = %.2f\n",);  // Correct the code
+   printf("Square root of %.2f = %.2f", num, root);  // Correct the code
    return 0;
 }
