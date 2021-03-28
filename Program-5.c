acee6357be21ccd11bc9579d0e0a496159594031
mohammedayan101 <ayanmohammed232@gmail.com>
2021-03-28 14:08:26 +0000

    5th one done

--- Program-5.c
+++ Program-5.c
@@ -1,13 +1,15 @@
 //INPUT: 5 7
-//OUTPUT: 7 13   6 14
-#include<stdio.h>// insert the missing line
+//OUTPUT: 7 13    6 14
+// insert the missing line
+#include<stdio.h>
  int main ()
 { 
-int i, j;     // insert the missing code
-printf("Enter the values of i and j\n");
+  int i,j;   // insert the missing code
+printf("Enter the values of i and j ");
 scanf("%d%d",&i,&j);
      i = i + 1;
-      j = (j + i + 1);  // correct the code
+      j = j + i + 1;  // correct the code
       printf("%d %d   ",i + 1, j - 1);
       printf("%d %d", i, j);
-return 0;}
+return 0;
+}
