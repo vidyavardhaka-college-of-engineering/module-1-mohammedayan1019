83f0c0ea51c0ef4341e54d95cf0180eebe86805d
mohammedayan101 <ayanmohammed232@gmail.com>
2021-03-28 14:59:28 +0000

    10th one done

--- Program-10.c
+++ Program-10.c
@@ -1,14 +1,15 @@
 //INPUT: 12
 //OUTPUT: Square root of 12.00 = 3.46
 #include <stdio.h>
-#include<math.h>// include the missing line
+#include<math.h>
+// include the missing line
 int main()
 {
    float num, root;
-   printf("enter the number\n");   // Correct the code
-   scanf("%f",&num);
+   printf("enter the number :");   // Correct the code
+   scanf("%f", &num);
    // Computes the square root of num and stores in root.
    root = sqrt(num);
-   printf("Square root of %.2f = %.2f", num, root);  // Correct the code
+   printf("Square root of %.2f = %.2f", num, root );  // Correct the code
    return 0;
 }
